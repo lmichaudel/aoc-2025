d20a08a2bf605ea32ec2e05253a891c1f57b41ad
lmichaudel <lucas.michaudel@gmail.com>
2025-12-02 10:41:05 +0100

    day 01

---
+++ day01/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int solve(char* file_path) {
+  int dial = 50; 
+  int zero_count = 0;
+
+  char line[128];
+  FILE *fp = fopen(file_path, "r");
+  while (fgets(line, sizeof(line), fp)) {
+    char turn;
+    int steps;
+
+    if (sscanf(line, " %c%d", &turn, &steps) == 2) {
+      int sign = turn == 'R' ? 1 : -1;
+      dial = (dial + sign*steps) % 100;
+      if (dial < 0) dial += 100;
+
+      if(0 == dial)
+        zero_count++;
+    }
+  }
+
+  return zero_count;
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+      fprintf(stderr, "Usage: %s <inputfile>\n", argv[0]);
+      return 1;
+  }
+  
+  printf("%s: %d\n", argv[1],  solve(argv[1]));
+
+  return 0;
+}
